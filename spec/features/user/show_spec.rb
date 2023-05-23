a528932de5d50b884f91a8e5367d9725c3dbd74f
margdaly <margaret.daly@gmail.com>
2023-05-23 15:25:33 -0600

    fix: Rubocop edit for user_1,2

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'User Show Page' do
   before(:each) do
-    @user_1 = User.create!(id: 1, name: 'Jimmy', email: 'jimmy@email.com', password: 'test', password_confirmation: 'test')
-    @user_2 = User.create!(id: 2, name: 'Bobby', email: 'bobby@email.com', password: 'test', password_confirmation: 'test')
+    @user1 = User.create!(id: 1, name: 'Jimmy', email: 'jimmy@email.com', password: 'test', password_confirmation: 'test')
+    @user2 = User.create!(id: 2, name: 'Bobby', email: 'bobby@email.com', password: 'test', password_confirmation: 'test')
 
-    visit '/users/1'
+    visit "/users/#{@user1.id}"
   end
 
   it "displays the user's name" do
@@ -16,12 +16,12 @@ RSpec.describe 'User Show Page' do
   it 'has a button to Discover Movies, which redirectes to the discover page' do
     expect(page).to have_button('Discover Movies')
     click_button 'Discover Movies'
-    expect(current_path).to eq("/users/#{@user_1.id}/discover")
+    expect(current_path).to eq("/users/#{@user1.id}/discover")
 
-    visit '/users/2'
+    visit "/users/#{@user2.id}"
     expect(page).to have_button('Discover Movies')
     click_button 'Discover Movies'
-    expect(current_path).to eq("/users/#{@user_2.id}/discover")
+    expect(current_path).to eq("/users/#{@user2.id}/discover")
   end
 
   it 'has section that lists viewing parties' do
@@ -29,7 +29,7 @@ RSpec.describe 'User Show Page' do
       expect(page).to have_content('Viewing Parties')
     end
 
-    visit '/users/2'
+    visit "/users/#{@user2.id}"
     within 'section#viewing_parties' do
       expect(page).to have_content('Viewing Parties')
     end
