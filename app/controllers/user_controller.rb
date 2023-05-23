83b0eefcab5501ff3ca70cb7cee9f81abac0f93f
Logan Cole <emtlogancole@gmail.com>
2023-05-23 11:29:30 -0500

    add session id to user#create

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,6 +10,7 @@ class UserController < ApplicationController
   def create
     user = User.new(user_params)
       if user.save
+        session[:user_id] = user.id
         redirect_to user_path(user)
         flash[:notice] = "Welcome #{user.name}!"
       else
